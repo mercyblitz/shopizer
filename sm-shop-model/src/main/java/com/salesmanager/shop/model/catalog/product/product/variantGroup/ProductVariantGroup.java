bbf39bf507a1054a0f31ef5db227334348ddca7b
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-09-08 08:33:29 +0800

    Remove code

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/product/product/variantGroup/ProductVariantGroup.java
+++
@@ -1,8 +0,0 @@
-package com.salesmanager.shop.model.catalog.product.product.variantGroup;
-import com.salesmanager.shop.model.entity.Entity;
-
-public class ProductVariantGroup extends Entity {
-
-	private static final long serialVersionUID = 1L;
-
-}
